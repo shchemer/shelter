1a94060e5d3b1b64eef0074dc0f8ad5e96f155a0
shchemer <gleb_2002@inbox.ru>
2023-04-04 22:13:21 +0300

    feat: add functionality for burger-menu

---
+++ shelter/assets/scripts/burger-menu.js
@@ -0,0 +1,14 @@
+let body = document.querySelector('body');
+let burgerMenu = document.querySelector('.burger-menu');
+let burgerIcon = document.querySelector('.burger-icon');
+burgerIcon.onclick = function() {
+	burgerIcon.classList.contains('burger-icon_active') ? body.style.overflow = "overlay" : body.style.overflow = "hidden";
+	burgerIcon.classList.toggle('burger-icon_active');
+}
+burgerMenu.onclick = function(e) {
+	if (e.target.classList.contains('burger-menu') || e.target.classList.contains('nav-menu__link')) 
+	{ 
+		burgerIcon.classList.remove('burger-icon_active');
+		body.style.overflow = "overlay";
+	}
+}
